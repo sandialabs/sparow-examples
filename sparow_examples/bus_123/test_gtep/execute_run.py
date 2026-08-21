64356b80eafca5a99b21cf5085bc801ffc6099b1
Matthew Viens <75225878+viens-code@users.noreply.github.com>
2026-08-20 19:35:46 -0500

    Updating to bus_123 instead of 123_bus

---
+++ sparow_examples/bus_123/test_gtep/execute_run.py
@@ -0,0 +1,46 @@
+import pytest
+from sparow.ef import ExtensiveFormSolver
+from sparow.ph import ProgressiveHedgingSolver
+import pyomo.opt
+from pyomo.common import unittest
+from sparow.sp.util import relax_second_stage
+import time
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+#from post_process_gtep_solution import post_process
+
+solvers = set(pyomo.opt.check_available_solvers("gurobi"))
+
+try:
+    from sparow_examples.bus_123.test_gtep import create_sp
+
+    dummy_available = True
+except:
+    dummy_available = False
+
+relaxations = {'scenario_A': {'relax_second_stage': True, 'unit_commitment': True}}
+rd = {k: v["relax_second_stage"] for k, v in relaxations.items()}
+
+script_start = time.perf_counter()
+sp = create_sp()
+sp.add_transformation(relax_second_stage, relax_dict=rd)
+solver = ExtensiveFormSolver()
+solver.set_options(solver="gurobi", loglevel="INFO")
+
+solve_start = time.perf_counter()
+res_munch = solver.solve_and_return_EF(sp)
+results = res_munch.solutions
+solve_end = time.perf_counter()
+
+results_dict = results.to_dict()
+
+soln = next(iter(results_dict["solutions"].values()))
+
+obj_val = soln["objectives"][0]["value"]
+total_end = time.perf_counter()
+print(f"Objective value: {obj_val}")
+print(f"Solve time (seconds): {solve_end - solve_start:.4f}")
+print(f"Total runtime (seconds): {total_end - script_start:.4f}")
+
+mod_object=res_munch.model.s['model','scenario_A']
+#post_process(mod_object)
